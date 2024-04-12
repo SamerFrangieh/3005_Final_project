ab30dd774cc5502d022b702bf1899b2d63b35f00
Samer Frangieh <samerfrangieh1@gmail.com>
2024-04-11 22:58:15 -0400

    yes

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -1,8 +1,26 @@
 INSERT INTO myapp_trainer (trainer_id, name, password, availability)
 VALUES (1, 'dhirran', 'dhirran', NULL);
 
+INSERT INTO myapp_trainer (trainer_id, name, password, availability)
+VALUES (2, 'Samer', 'samer', NULL);
+
+INSERT INTO myapp_trainer (trainer_id, name, password, availability)
+VALUES (2, 'James', 'james', NULL);
+
+INSERT INTO myapp_trainer (trainer_id, name, password, availability)
+VALUES (2, 'Amir', 'amir', NULL);
+
+INSERT INTO myapp_trainer (trainer_id, name, password, availability)
+VALUES (2, 'Cristina', 'cristina', NULL);
+
+INSERT INTO myapp_trainer (trainer_id, name, password, availability)
+VALUES (2, 'Jenny', 'jenny', NULL);
+
 INSERT INTO myapp_admin (admin_id, name, password)
 VALUES (1, 'dhirran', 'dhirran');
+
+INSERT INTO myapp_admin (admin_id, name, password)
+VALUES (2, 'admin', 'admin');
 
 INSERT INTO myapp_member (member_id, name, password, health_metrics, height, weight, goal_weight, weeks_to_goal, diastolic_bp, systolic_bp, fitness_goal)
 VALUES (1, 'dhirran', 'dhirran', '{}', 180.00, 180.00, NULL, NULL, NULL, NULL, 'Lose weight');

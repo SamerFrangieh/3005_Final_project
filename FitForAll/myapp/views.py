5ad960cac49b2ef3e2e23d14090aee4a96a27c88
Samer Frangieh <samerfrangieh1@gmail.com>
2024-04-13 00:15:15 -0400

    excersize routines done

--- FitForAll/myapp/views.py
+++ FitForAll/myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.db import connection
+import os
  # Prints the last executed query
 # #from .models import Book
 # def member_login(request):
@@ -262,6 +263,159 @@ def dashboard(request):
     else:
         bp_health = 'Consult a doctor'
 
+    #The workout schedules
+
+    mon = ""
+    tue = ''
+    wed = ''
+    thu = ''
+    fri = ''
+    sat = ''
+    sun = ''
+
+    if member.fitness_goal == 'gain_muscle':
+        
+        if member.act_levels == '1-3 x times a week':
+            mon= 'Day 1: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            tue = 'rest'
+            wed = 'Day 2: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            thu = 'rest'
+            fri = "Day 3: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            sat = 'rest'
+            sun = 'rest'
+        elif member.act_levels == '3-5 x times a week':
+            mon= 'Day 1: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            tue = 'rest'
+            wed = 'Day 2: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            thu = 'Day 3: Arm Workout Day: Biceps and Triceps//Barbell Curl - 4 sets of 8-12 reps//Tricep Dips - 4 sets of 8-12 reps//Hammer Curls - 3 sets of 10-12 reps//Skull Crushers - 3 sets of 8-12 reps//Preacher Curl - 3 sets of 8-12 reps'
+            fri = "Day 4: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            sat = 'rest'
+            sun = 'rest'
+        elif member.act_levels == '5-6 x times a week':
+            mon= 'Day 1: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            tue = 'Day 2: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            wed = "Day 3: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            thu = 'Day 4: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            fri = 'Day 5: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            sat = "Day 6: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            sun = 'rest'
+        elif member.act_levels =='6-7 x times a week':
+            mon= 'Day 1: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            tue = 'Day 2: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            wed = "Day 3: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            thu = 'Day 4: Push (Chest, Shoulders, and Triceps)//Bench Press - 4 sets of 8-12 reps,\n//Overhead Press - 3 sets of 8-12 reps,\n//Incline Dumbbell Press - 3 sets of 8-12 reps,\n//Lateral Raises - 3 sets of 12-15 reps,\n//Tricep Dips - 3 sets of 10-15 reps,//Tricep Pushdowns - 3 sets of 10-15 reps\n'
+            fri = 'Day 5: Pull (Back, Biceps)//Deadlifts - 3 sets of 6-8 reps//Pull-ups - 3 sets of as many reps as possible//Barbell Rows - 3 sets of 8-12 reps//Face Pulls - 3 sets of 12-15 reps//Hammer Curls - 3 sets of 10-12 reps//Barbell Curls - 3 sets of 8-12 reps'
+            sat = "Day 6: Legs (Quads, Hamstrings, and Calves)//Squats - 4 sets of 8-12 reps//Leg Press - 3 sets of 10-12 reps//Romanian Deadlifts - 3 sets of 8-12 reps//Leg Curls - 3 sets of 10-12 reps//Calf Raises - 5 sets of 12-15 reps"
+            sun = 'Day 7: Arm Workout Day: Biceps and Triceps//Barbell Curl - 4 sets of 8-12 reps//Tricep Dips - 4 sets of 8-12 reps//Hammer Curls - 3 sets of 10-12 reps//Skull Crushers - 3 sets of 8-12 reps//Preacher Curl - 3 sets of 8-12 reps'
+        
+    elif member.fitness_goal == 'lose_weight':
+        if member.act_levels == '1-3 x times a week':
+            mon= 'Day 1: Cardio and Core//Treadmill Running - 30 minutes at a moderate pace//Cycling - 20 minutes at a vigorous pace//Russian Twists - 4 sets of 15 reps each side//Leg Raises - 4 sets of 12 reps'
+            tue = 'rest'
+            wed = 'Day 2: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            thu = 'rest'
+            fri = "Day 3: High-Intensity Interval Training (HIIT)//Sprints - 10 rounds of 30 seconds sprint/30 seconds rest//Burpees - 5 sets of 20 seconds on/40 seconds rest//Jump Rope - 10 minutes with intervals of 1 minute on/1 minute off"
+            sat = 'rest'
+            sun = 'rest'
+        elif member.act_levels == '3-5 x times a week':
+            mon= 'Day 1: Cardio and Core//Treadmill Running - 30 minutes at a moderate pace//Cycling - 20 minutes at a vigorous pace//Russian Twists - 4 sets of 15 reps each side//Leg Raises - 4 sets of 12 reps'
+            tue = 'rest'
+            wed = 'Day 2: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            thu = 'rest'
+            fri = "Day 3: High-Intensity Interval Training (HIIT)//Sprints - 10 rounds of 30 seconds sprint/30 seconds rest//Burpees - 5 sets of 20 seconds on/40 seconds rest//Jump Rope - 10 minutes with intervals of 1 minute on/1 minute off"
+            sat = 'Day 4: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            sun = 'rest'
+        elif member.act_levels == '5-6 x times a week':
+            mon= 'Day 1: Cardio and Core//Treadmill Running - 30 minutes at a moderate pace//Cycling - 20 minutes at a vigorous pace//Russian Twists - 4 sets of 15 reps each side//Leg Raises - 4 sets of 12 reps'
+            tue = 'Day 2: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            wed = 'Day 3: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            thu = 'rest'
+            fri = "Day 4: High-Intensity Interval Training (HIIT)//Sprints - 10 rounds of 30 seconds sprint/30 seconds rest//Burpees - 5 sets of 20 seconds on/40 seconds rest//Jump Rope - 10 minutes with intervals of 1 minute on/1 minute off"
+            sat = 'Day 5: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            sun = 'rest'
+        elif member.act_levels =='6-7 x times a week':
+            mon= 'Day 1: Cardio and Core//Treadmill Running - 30 minutes at a moderate pace//Cycling - 20 minutes at a vigorous pace//Russian Twists - 4 sets of 15 reps each side//Leg Raises - 4 sets of 12 reps'
+            tue = 'Day 2: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            wed = 'Day 3: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            thu = 'Day 6: Active RecoveryYoga - 60 minutes focusing on flexibility and core//Light Walking - 30 minutes at a gentle pace'
+            fri = "Day 4: High-Intensity Interval Training (HIIT)//Sprints - 10 rounds of 30 seconds sprint/30 seconds rest//Burpees - 5 sets of 20 seconds on/40 seconds rest//Jump Rope - 10 minutes with intervals of 1 minute on/1 minute off"
+            sat = 'Day 5: Full Body Strength//Squats - 4 sets of 10-12 reps//Bench Press - 4 sets of 10-12 reps//Deadlifts - 3 sets of 10 reps//Pull-ups - 3 sets of AMRAP//Plank - 3 sets of 1 min hold'
+            sun = 'rest'
+
+    elif member.fitness_goal == 'improve_endurance':
+        if member.act_levels == '1-3 x times a week':
+            mon = "Day 1: Cardio Intervals//Treadmill or Outdoor Running - 30 minutes of interval training (1 min fast/2 min slow)"
+            tue = "rest"
+            wed = "Day 2: Full Body Circuit//Bodyweight Exercises (Push-ups, Pull-ups, Squats) - 3 rounds of 15 reps each"
+            thu = "rest"
+            fri = "Day 3: Long Duration Cardio//Cycling or Swimming - 45 minutes at a steady pace"
+            sat = "rest"
+            sun = "rest"
+
+        elif member.act_levels == '3-5 x times a week':
+            mon = "Day 1: High Intensity Interval Training//HIIT - 20 minutes (30s high intensity/30s low intensity)"
+            tue = "rest"
+            wed = "Day 2: Strength and Endurance Circuit//Mix of Weight Lifting and Bodyweight Exercises - 3 sets of 12 reps"
+            thu = "Day 3: Cardio Intervals//Rowing Machine or Jump Rope - 20 minutes of interval training"
+            fri = "rest"
+            sat = "Day 4: Long Duration Cardio//Jogging - 60 minutes at a moderate pace"
+            sun = "rest"
+
+        elif member.act_levels == '5-6 x times a week':
+            mon = "Day 1: Cardio Intervals//Treadmill Sprints - 30 minutes of interval training (1 min sprint/2 min walk)"
+            tue = "Day 2: Circuit Training//Full body circuit with resistance bands - 3 circuits of 10 mins each"
+            wed = "rest"
+            thu = "Day 3: Strength Training//Bodyweight strength exercises - 4 sets of 10-12 reps"
+            fri = "Day 4: Cardio Endurance//Steady State Cycling - 50 minutes at a moderate intensity"
+            sat = "Day 5: Active Recovery//Yoga or light stretching - 30 minutes"
+            sun = "rest"
+
+        elif member.act_levels == '6-7 x times a week':
+            mon = "Day 1: Interval Training//Treadmill intervals - 25 minutes (3 min run/2 min walk)"
+            tue = "Day 2: Circuit Training//Kettlebell circuit - 4 sets of 15 reps"
+            wed = "Day 3: Endurance Cardio//Long distance running - 60 minutes at a steady pace"
+            thu = "Day 4: High-Intensity Bodyweight//Tabata style - 20 minutes (20s work/10s rest)"
+            fri = "Day 5: Strength Focus//Compound lifting - Squats, Deadlifts, Bench Press - 3 sets of 8-12 reps"
+            sat = "Day 6: Mixed Cardio//Rowing and Cycling - 45 minutes total"
+            sun = "Day 7: Active Rest//Stretching and foam rolling - 30 minutes"
+
+    if member.fitness_goal == 'increase_flexibility':
+        if member.act_levels == '1-3 x times a week':
+            mon = "Day 1: Yoga Stretching//Full body yoga - 30 minutes"
+            tue = "rest"
+            wed = "Day 2: Dynamic Stretching//Full body dynamic stretches - 20 minutes"
+            thu = "rest"
+            fri = "Day 3: Pilates//Beginner Pilates session - 30 minutes"
+            sat = "rest"
+            sun = "rest"
+
+        elif member.act_levels == '3-5 x times a week':
+            mon = "Day 1: Yoga Stretching//Full body yoga - 45 minutes"
+            tue = "rest"
+            wed = "Day 2: Dynamic Stretching//Leg and hip dynamic stretches - 20 minutes"
+            thu = "Day 3: Tai Chi//Beginner Tai Chi class - 30 minutes"
+            fri = "rest"
+            sat = "Day 4: Pilates//Intermediate Pilates session - 40 minutes"
+            sun = "rest"
+
+        elif member.act_levels == '5-6 x times a week':
+            mon = "Day 1: Yoga Stretching//Advanced yoga poses - 45 minutes"
+            tue = "Day 2: Pilates//Core-focused Pilates - 40 minutes"
+            wed = "rest"
+            thu = "Day 3: Dynamic Stretching//Full body dynamic stretches - 30 minutes"
+            fri = "Day 4: Tai Chi//Intermediate Tai Chi session - 45 minutes"
+            sat = "Day 5: Active Recovery//Light yoga and meditation - 30 minutes"
+            sun = "rest"
+
+        elif member.act_levels == '6-7 x times a week':
+            mon = "Day 1: Yoga Stretching//Intensive yoga session - 60 minutes"
+            tue = "Day 2: Dynamic Stretching//Sports specific stretches - 30 minutes"
+            wed = "Day 3: Pilates//Advanced Pilates session - 45 minutes"
+            thu = "Day 4: Yoga Stretching//Power yoga - 45 minutes"
+            fri = "Day 5: Tai Chi//Advanced Tai Chi practice - 60 minutes"
+            sat = "Day 6: Dynamic Stretching//Injury prevention stretches - 30 minutes"
+            sun = "Day 7: Active Rest//Gentle yoga and deep breathing - 30 minutes"
+
 
 
     if request.method == 'POST':
@@ -274,10 +428,12 @@ def dashboard(request):
         member.age = request.POST.get('Age')
         member.save()
         messages.success(request, "Profile updated successfully!")
-        context = {'member': member, 'bmi': bmi, 'bmi_category': bmi_category, 'bp_health': bp_health, 'bp': bp, 'bmr': bmr,'rec_bmr': rec_bmr}
+        context = {'member': member, 'bmi': bmi, 'bmi_category': bmi_category, 'bp_health': bp_health, 'bp': bp, 'bmr': bmr,'rec_bmr': rec_bmr,
+                   'mon': mon, 'tue': tue,'wed': wed, 'thu': thu, 'fri': fri,'sat': sat,'sun': sun}
         return render(request, 'myapp/dashboard/index.html', context)
 
-    context = {'member': member, 'bmi': bmi, 'bmi_category': bmi_category, 'bp_health': bp_health, 'bp': bp, 'bmr': bmr,'rec_bmr': rec_bmr}
+    context = {'member': member, 'bmi': bmi, 'bmi_category': bmi_category, 'bp_health': bp_health, 'bp': bp, 'bmr': bmr,'rec_bmr': rec_bmr,
+               'mon': mon, 'tue': tue,'wed': wed, 'thu': thu, 'fri': fri,'sat': sat,'sun': sun}
     return render(request, 'myapp/dashboard/index.html', context)
 
 
